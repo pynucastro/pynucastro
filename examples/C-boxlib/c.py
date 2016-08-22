39ac644ebf414a4b82e43bc31a09a23900d72a62
dwillcox <dwillcox@electra.iacs.stonybrook.edu>
2016-08-21 21:57:03 -0400

    Add c12+alpha->o16 to C-boxlib and URCA-boxlib examples.

--- examples/C-boxlib/c.py
+++ examples/C-boxlib/c.py
@@ -5,6 +5,7 @@ import reaclib
 files = ["c12-c12a-ne20-cf88",
          "c12-c12n-mg23-cf88",
          "c12-c12p-na23-cf88",
+         "c12-ag-o16-nac2",
          "n--p-wc12"]
 
 rc = reaclib.RateCollection(files)
--- examples/URCA-boxlib/urca.py
+++ examples/URCA-boxlib/urca.py
@@ -5,6 +5,7 @@ import reaclib
 files = ["c12-c12a-ne20-cf88",
          "c12-c12n-mg23-cf88",
          "c12-c12p-na23-cf88",
+         "c12-ag-o16-nac2",
          "na23--ne23-toki",
          "ne23--na23-toki",
          "n--p-wc12"]
