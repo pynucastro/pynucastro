40d85e16eed3938a200137db92b080373b2c7e19
dwillcox <dwillcox@electra.iacs.stonybrook.edu>
2016-09-01 13:27:10 -0400

    Updated the C_f90 network to include helium capture on c12.

--- examples/C_f90/c.py
+++ examples/C_f90/c.py
@@ -5,6 +5,7 @@ import pyreaclib
 files = ["c12-c12a-ne20-cf88",
          "c12-c12n-mg23-cf88",
          "c12-c12p-na23-cf88",
+         "c12-ag-o16-nac2",
          "n--p-wc12"]
 
 pyreaclib.make_network(files, 'sundials')
