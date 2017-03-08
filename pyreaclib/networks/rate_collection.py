4754e84b53e8aa58cb10175954ac046a152d1a32
Michael Zingale <michael.zingale@stonybrook.edu>
2017-03-08 12:58:42 -0500

    import from ipywidgets

--- pyreaclib/networks/rate_collection.py
+++ pyreaclib/networks/rate_collection.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 import glob
 import os
 
-from IPython.html.widgets import interact
+from ipywidgets import interact
 
 import functools
 from operator import mul
