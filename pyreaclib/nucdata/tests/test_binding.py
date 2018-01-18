ad0151eee0027237c8cdd433ef2f24bfa47af5df
dwillcox <eugene.willcox@gmail.com>
2018-01-18 16:08:57 -0500

    Add some more binding energy table tests.

--- pyreaclib/nucdata/tests/test_binding.py
+++ pyreaclib/nucdata/tests/test_binding.py
@@ -29,3 +29,17 @@ class TestAME(object):
         assert nuc.n == 1
         assert nuc.nucbind == 1.112283
 
+        nuc = self.bintable.get_nuclide(n=5, z=6)
+        assert nuc.z == 6
+        assert nuc.n == 5
+        assert nuc.nucbind == 6.676456
+
+        nuc = self.bintable.get_nuclide(n=17, z=23)
+        assert nuc.z == 23
+        assert nuc.n == 17
+        assert nuc.nucbind == 7.317
+
+        nuc = self.bintable.get_nuclide(n=90, z=78)
+        assert nuc.z == 78
+        assert nuc.n == 90
+        assert nuc.nucbind == 7.773605
