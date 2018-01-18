c6be99acb923c536147e5d2738e2a5df0bc6c568
dwillcox <eugene.willcox@gmail.com>
2018-01-18 13:24:48 -0500

    Add test for reading nuclei properties based on the chapters example.

--- pyreaclib/networks/tests/test_starkiller_net.py
+++ pyreaclib/networks/tests/test_starkiller_net.py
@@ -60,3 +60,93 @@ class TestFortranNetwork(object):
 
         ydot = self.fn.ydot_string(self.fn.rates[1])
         assert ydot == answer
+
+class TestReaclibChapterNetwork(object):
+    @classmethod
+    def setup_class(cls):
+        """ this is run once for each class before any tests """
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        """ this is run once for each class after all tests """
+        pass
+
+    def setup_method(self):
+        """ this is run before each test """
+
+        files = ["b17-nnn-c14-wc12",
+                 "he3-he3pp-he4-nacr",
+                 "he4-aag-c12-fy05",
+                 "he4-npahe3-li7-mafo",
+                 "he4-pphe3-he3-nacr",
+                 "he6-gnn-he4-cf88",
+                 "li7-tnna-he4-mafo",
+                 "n--p-wc12",
+                 "p-ng-d-an06",
+                 "t-gn-d-nk06",
+                 "t-pn-he3-de04"]
+
+        self.fn = networks.StarKillerNetwork(files)
+
+        self.n = rates.Nucleus("n")
+        self.p = rates.Nucleus("p")
+        self.d = rates.Nucleus("d")
+        self.t = rates.Nucleus("t")
+        self.he3 = rates.Nucleus("he3")
+        self.he4 = rates.Nucleus("he4")
+        self.he6 = rates.Nucleus("he6")
+        self.li7 = rates.Nucleus("li7")
+        self.b17 = rates.Nucleus("b17")
+        self.c12 = rates.Nucleus("c12")
+        self.c14 = rates.Nucleus("c14")
+
+    def teardown_method(self):
+        """ this is run after each test """
+        self.tf = None
+
+    def test_nuclei(self):
+        """ test the nuclei are correctly identified for this network """
+        assert self.n.N == 1
+        assert self.n.Z == 0
+        assert self.n.A == 1
+
+        assert self.p.N == 0
+        assert self.p.Z == 1
+        assert self.p.A == 1
+
+        assert self.d.N == 1
+        assert self.d.Z == 1
+        assert self.d.A == 2
+
+        assert self.t.N == 2
+        assert self.t.Z == 1
+        assert self.t.A == 3
+
+        assert self.he3.N == 1
+        assert self.he3.Z == 2
+        assert self.he3.A == 3
+
+        assert self.he4.N == 2
+        assert self.he4.Z == 2
+        assert self.he4.A == 4
+
+        assert self.he6.N == 4
+        assert self.he6.Z == 2
+        assert self.he6.A == 6
+
+        assert self.li7.N == 4
+        assert self.li7.Z == 3
+        assert self.li7.A == 7
+
+        assert self.b17.N == 12
+        assert self.b17.Z == 5
+        assert self.b17.A == 17
+
+        assert self.c12.N == 6
+        assert self.c12.Z == 6
+        assert self.c12.A == 12
+
+        assert self.c14.N == 8
+        assert self.c14.Z == 6
+        assert self.c14.A == 14
