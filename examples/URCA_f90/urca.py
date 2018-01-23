9023d709d742a250279a3949b7e584bbd9f9dd4e
dwillcox <eugene.willcox@gmail.com>
2018-01-23 02:03:52 -0500

    Add Base Fortran VODE example URCA_f90.

---
+++ examples/URCA_f90/urca.py
@@ -0,0 +1,18 @@
+# C-burning with A=23 URCA rate module generator
+
+from pynucastro.networks import BaseFortranNetwork
+
+files = ["c12-c12a-ne20-cf88",
+         "c12-c12n-mg23-cf88",
+         "c12-c12p-na23-cf88",
+         "na23--ne23-toki",
+         "ne23--na23-toki",
+         "n--p-wc12"]
+
+urca_net = BaseFortranNetwork(files)
+urca_net.write_network(use_cse=True)
+
+
+
+
+
