430483342b51f3cc24939e39b88747d0e987afa1
dwillcox <eugene.willcox@gmail.com>
2018-01-26 01:33:12 -0500

    Remove sundials network class.

--- pynucastro/networks/__init__.py
+++ pynucastro/networks/__init__.py
@@ -2,4 +2,3 @@ from .rate_collection import RateCollection, Composition, Explorer
 from .python_network import PythonNetwork
 from .base_fortran_network import BaseFortranNetwork
 from .starkiller_network import StarKillerNetwork
-from .sundials_network import SundialsNetwork
