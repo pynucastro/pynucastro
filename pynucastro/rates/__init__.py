d4ffd507941f99395e1d7e8471b63cd2216f9e61
dwillcox <eugene.willcox@gmail.com>
2018-02-12 16:15:53 -0500

    Revise comments to include RateLibrary, RateFilter classes.

--- pynucastro/rates/__init__.py
+++ pynucastro/rates/__init__.py
@@ -1,4 +1,4 @@
-"""The core routines needed to read and interpret a nuclear reaction rate"""
+"""The core routines needed to read and interpret nuclear reaction rates"""
 
 from .rate import Tfactors, Library, Rate, RateFilter, UnsupportedNucleus, Nucleus, list_known_rates
 
