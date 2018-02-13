1cd3622c45e40dd23dc5e11c0c8e4e352d9334ce
dwillcox <eugene.willcox@gmail.com>
2018-02-13 17:26:07 -0500

    Revert "Deploy pynucastro/pynucastro to github.com/pynucastro/pynucastro.git:gh-pages"
    This reverts commit ac32dd5e3db31eb114d571c0be69a38b42440281.

--- examples/triple-alpha/README.md
+++
@@ -1,5 +0,0 @@
-# triple-alpha
-
-Python network for triple-alpha fusion to C-12 and the reverse rate.
-
-Now with the correct prefactor for C-12 photodisintegration.
--- pynucastro/nucdata/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Properties of nuclei needed in construction reaction rates (in
-particular, the binding energies)
-
-"""
-
-from .binding_nuclide import BindingNuclide
-from .binding_table import BindingTable
-from .elements import Element, UnidentifiedElement, PeriodicTable
