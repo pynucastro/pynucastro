383560e40268eedb56d5030f654f660465957a47
dwillcox <eugene.willcox@gmail.com>
2018-03-01 01:24:41 -0500

    Add example building a subch network using the Library.linking_nuclei feature.

---
+++ examples/subch/subch_forward.py
@@ -0,0 +1,19 @@
+# C-burning with A=23 URCA rate module generator
+
+import pynucastro as pyna
+from pynucastro.networks import StarKillerNetwork
+
+library_file = "20180228default2"
+mylibrary = pyna.rates.Library(library_file)
+
+all_nuclei = ["he4", "c12", "o16", "n14", "f18", "ne21", "p", "n13", "ne20"]
+
+subCh = mylibrary.linking_nuclei(all_nuclei, with_reverse=False)
+
+net = StarKillerNetwork(libraries=subCh)
+net.write_network()
+
+
+
+
+
