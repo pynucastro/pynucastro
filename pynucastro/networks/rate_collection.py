76866c6731ebf869c12ad5afdd1f39daeda1c7e2
Kiran Eiden <keiden@eidensystems.com>
2019-07-21 20:31:18 -0700

    Added gridplot

--- pynucastro/networks/rate_collection.py
+++ pynucastro/networks/rate_collection.py
@@ -4,6 +4,7 @@ rates that together make up a network."""
 # Common Imports
 from __future__ import print_function
 
+import warnings
 import functools
 import math
 import numpy as np
@@ -15,7 +16,8 @@ from ipywidgets import interact
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-#from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.ticker import MaxNLocator
 import networkx as nx
 
 # Import Rate
@@ -230,6 +232,51 @@ class RateCollection(object):
             rvals[r] = yfac * val
 
         return rvals
+        
+    def evaluate_ydots(self, rho, T, composition):
+        """evaluate net rate of change of molar abundance for each nucleus
+        for a specific density, temperature, and composition"""
+        
+        rvals = self.evaluate_rates(rho, T, composition)
+        ydots = dict()
+        
+        for nuc in self.unique_nuclei:
+            
+            # Rates that consume / produce nuc
+            consuming_rates = self.nuclei_consumed[nuc]
+            producing_rates = self.nuclei_produced[nuc]
+            # Number of nuclei consumed / produced
+            nconsumed = (r.reactants.count(nuc) for r in consuming_rates)
+            nproduced = (r.products.count(nuc) for r in producing_rates)
+            # Multiply each rate by the count
+            consumed = (c * rvals[r] for c, r in zip(nconsumed, consuming_rates))
+            produced = (c * rvals[r] for c, r in zip(nproduced, producing_rates))
+            # Net change is difference between produced and consumed
+            ydots[nuc] = sum(produced) - sum(consumed)
+            
+        return ydots
+        
+    def evaluate_activity(self, rho, T, composition):
+        """sum over"""
+        
+        rvals = self.evaluate_rates(rho, T, composition)
+        act = dict()
+        
+        for nuc in self.unique_nuclei:
+            
+            # Rates that consume / produce nuc
+            consuming_rates = self.nuclei_consumed[nuc]
+            producing_rates = self.nuclei_produced[nuc]
+            # Number of nuclei consumed / produced
+            nconsumed = (r.reactants.count(nuc) for r in consuming_rates)
+            nproduced = (r.products.count(nuc) for r in producing_rates)
+            # Multiply each rate by the count
+            consumed = (c * rvals[r] for c, r in zip(nconsumed, consuming_rates))
+            produced = (c * rvals[r] for c, r in zip(nproduced, producing_rates))
+            # Net change is difference between produced and consumed
+            act[nuc] = sum(produced) + sum(consumed)
+            
+        return act
 
     def network_overview(self):
         """ return a verbose network overview """
@@ -386,13 +433,235 @@ class RateCollection(object):
         else:
             plt.tight_layout()
             plt.savefig(outfile, dpi=dpi)
+    
+    @staticmethod        
+    def _safelog(arr, small):
+        
+        arr = np.copy(arr)
+        if np.any(arr < 0.0):
+            raise ValueError("Negative values not allowed for logscale - try symlog instead.")
+        zeros = arr == 0.0
+        arr[zeros] = min(small, arr[~zeros].min() / 10)
+        return np.log10(arr)
+        
+    @staticmethod
+    def _symlog(arr, linthresh=1.0):
+        
+        assert linthresh >= 1.0
+        neg = arr < 0.0
+        arr = np.abs(arr)
+        needslog = arr > linthresh
+        
+        arr[needslog] = np.log10(arr[needslog]) + linthresh
+        arr[neg] *= -1
+        return arr
+    
+    @staticmethod    
+    def _scale(arr, minval=None, maxval=None):
+        
+        if minval is None: minval = arr.min()
+        if maxval is None: maxval = arr.max()
+        if minval != maxval:
+            scaled = (arr - minval) / (maxval - minval)
+        else:
+            scaled = np.zeros_like(arr)
+        scaled[scaled < 0.0] = 0.0
+        scaled[scaled > 1.0] = 1.0
+        return scaled
+            
+    def gridplot(self, comp=None, color_field="X", rho=None, T=None, **kwargs):
+        """
+        Plot nuclides as cells on a grid of Z vs. N, colored by *color_field*. If called
+        without a composition, the function will just plot the grid with no color field.
+        
+        :param comp: Composition of the environment.
+        :param color_field: Field to color by. Must be one of 'X' (mass fraction),
+            'Y' (molar abundance), 'Xdot' (time derivative of X), 'Ydot' (time
+            derivative of Y), or 'activity' (sum of contributions to Ydot of
+            all rates, ignoring sign).
+        :param rho: Density to evaluate rates at. Needed for fields involving time
+            derivatives.
+        :param T: Temperature to evaluate rates at. Needed for fields involving time
+            derivatives.
+        
+        :Keyword Arguments:
+        
+            - *scale* -- One of 'linear', 'log', and 'symlog'. Linear by default.
+            - *small* -- If using logarithmic scaling, zeros will be replaced with
+              this value. 1e-30 by default.
+            - *linthresh* -- Linearity threshold for symlog scaling.
+            - *filter_function* -- A callable to filter Nucleus objects with. Should
+              return *True* if the nuclide should be plotted.
+            - *outfile* -- Output file to save the plot to. The plot will be shown if
+              not specified.
+            - *dpi* -- DPI to save the image file at.
+            - *cmap* -- Name of the matplotlib colormap to use. Default is 'magma'.
+            - *edgecolor* -- Color of grid cell edges.
+            - *area* -- Area of the figure without the colorbar, in square inches. 64
+              by default.
+            - *no_axes* -- Set to *True* to omit axis spines.
+            - *no_ticks* -- Set to *True* to omit tickmarks.
+            - *no_cbar* -- Set to *True* to omit colorbar.
+            - *cbar_label* -- Colorbar label.
+            - *cbar_bounds* -- Explicit colorbar bounds.
+            - *cbar_format* -- Format string or Formatter object for the colorbar ticks.
+        """
+            
+        # Process kwargs
+        outfile = kwargs.pop("outfile", None)
+        scale = kwargs.pop("scale", "linear")
+        cmap = kwargs.pop("cmap", "magma")
+        edgecolor = kwargs.pop("edgecolor", "grey")
+        small = kwargs.pop("small", 1e-30)
+        area = kwargs.pop("area", 64)
+        no_axes = kwargs.pop("no_axes", False)
+        no_ticks = kwargs.pop("no_ticks", False)
+        no_cbar = kwargs.pop("no_cbar", False)
+        cbar_label = kwargs.pop("cbar_label", None)
+        cbar_format = kwargs.pop("cbar_format", None)
+        cbar_bounds = kwargs.pop("cbar_bounds", None)
+        filter_function = kwargs.pop("filter_function", None)
+        dpi = kwargs.pop("dpi", 100)
+        linthresh = kwargs.pop("linthresh", 1.0)
+        
+        if kwargs: warnings.warn("Unrecognized keyword arguments: {}".format(kwargs.keys()))
+        
+        # Get figure, colormap
+        fig, ax = plt.subplots()
+        cmap = mpl.cm.get_cmap(cmap)
+        
+        # Get nuclei and all 3 numbers
+        nuclei = self.unique_nuclei
+        if filter_function is not None:
+            nuclei = list(filter(filter_function, nuclei))
+        Ns = np.array([n.N for n in nuclei])
+        Zs = np.array([n.Z for n in nuclei])
+        As = Ns + Zs
+        
+        # Compute weights
+        color_field = color_field.lower()
+        if color_field not in {"x", "y", "ydot", "xdot", "activity"}:
+            raise ValueError("Invalid color field: '{}'".format(color_field))
+        
+        if comp is None:
+            
+            values = np.zeros(len(nuclei))
+            
+        elif color_field == "x":
+            
+            values = np.array([comp.X[nuc] for nuc in nuclei])
+            
+        elif color_field == "y":
+            
+            ys = comp.get_molar()
+            values = np.array([ys[nuc] for nuc in nuclei])
+            
+        elif color_field in {"ydot", "xdot"}:
+            
+            if rho is None or T is None:
+                raise ValueError("Need both rho and T to evaluate rates!")
+            ydots = self.evaluate_ydots(rho, T, comp)
+            values = np.array([ydots[nuc] for nuc in nuclei])
+            if color_field == "xdot": values *= As
+            
+        elif color_field == "activity":
+            
+            if rho is None or T is None:
+                raise ValueError("Need both rho and T to evaluate rates!")
+            act = self.evaluate_activity(rho, T, comp)
+            values = np.array([act[nuc] for nuc in nuclei])
+            
+        if scale == "log": values = self._safelog(values, small)
+        elif scale == "symlog": values = self._symlog(values, linthresh)
+        
+        if cbar_bounds is None:
+            cbar_bounds = values.min(), values.max()
+        weights = self._scale(values, *cbar_bounds)
+        
+        # Plot a square for each nucleus
+        for nuc, weight in zip(nuclei, weights):
+            
+            square = plt.Rectangle((nuc.N, nuc.Z), width=1, height=1,
+                    facecolor=cmap(weight), edgecolor=edgecolor)
+            ax.add_patch(square)
+        
+        # Set limits, plot appearance
+        maxN, minN = max(Ns), min(Ns)
+        maxZ, minZ = max(Zs), min(Zs)
+        
+        plt.xlim(minN, maxN + 1.1)
+        plt.ylim(minZ, maxZ + 1.1)
+        
+        rat = (maxN - minN) / (maxZ - minZ)
+        width = np.sqrt(area * rat)
+        height = area / width
+        fig.set_size_inches(width, height)
+         
+        plt.xlabel(r"N $\rightarrow$")
+        plt.ylabel(r"Z $\rightarrow$")
+        
+        if no_axes or no_ticks:
+            
+            plt.tick_params \
+            (
+                axis = 'both',  
+                which = 'both',      
+                bottom = False,      
+                left = False,
+                labelbottom = False,
+                labelleft = False
+            )
+            
+        else:
+            
+            ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+            ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+        
+        ax.spines['right'].set_visible(False)
+        ax.spines['top'].set_visible(False)
+        if no_axes:
+            ax.spines['bottom'].set_visible(False)
+            ax.spines['left'].set_visible(False)
+            
+        for item in [ax.xaxis.label, ax.yaxis.label]:
+            item.set_fontsize(20)
+        for item in (ax.get_xticklabels() + ax.get_xticklabels()):
+            item.set_fontsize(16)
+        
+        # Colorbar stuff
+        if not no_cbar and comp is not None:
+            
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes('right', size='3.5%', pad=0.1)
+            cbar_norm = mpl.colors.Normalize(*cbar_bounds)
+            smap = mpl.cm.ScalarMappable(norm=cbar_norm, cmap=cmap)
+            
+            if not cbar_label:
+                
+                capfield = color_field.capitalize()
+                if scale == "log":
+                    cbar_label = "log[{}]".format(capfield)
+                elif scale == "symlog":
+                    cbar_label = "symlog[{}]".format(capfield)
+                else:
+                    cbar_label = capfield
+                
+            cbar = fig.colorbar(smap, cax=cax, orientation="vertical", format=cbar_format)
+            cbar.set_label(cbar_label, size=20)
+            cbar.ax.tick_params(labelsize=16)
+        
+        # Show or save
+        if outfile is None:
+            plt.show()
+        else:
+            plt.tight_layout()
+            plt.savefig(outfile, dpi=dpi)
 
     def __repr__(self):
         string = ""
         for r in self.rates:
             string += "{}\n".format(r.string)
         return string
-
 
 class Explorer(object):
     """ interactively explore a rate collection """
