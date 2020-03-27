adb82f87427f003ebb7e004906faae9c7796886e
Michael Zingale <michael.zingale@stonybrook.edu>
2020-03-27 10:49:41 -0400

    some updates for new starkiller conventions

--- pynucastro/networks/starkiller_network.py
+++ pynucastro/networks/starkiller_network.py
@@ -146,3 +146,23 @@ class StarKillerNetwork(BaseFortranNetwork):
             of.write('# {}\n'.format(n))
             xin = [self.fmt_to_dp_f90(xcomp) for j in range(size_test)]
             of.write('{}\n'.format(' '.join(xin)))
+
+    def _write_network(self, use_cse=False):
+        """
+        This writes the RHS, jacobian and ancillary files for the system of ODEs that
+        this network describes, using the template files.
+        """
+
+        super()._write_network(use_cse=use_cse)
+
+        # create a .net file with the nuclei properties
+        with open("pynucastro.net", "w") as of:
+            for nuc in self.unique_nuclei:
+                of.write("{:25} {:6} {:6.1f} {:6.1f}\n".format(
+                    nuc.spec_name, nuc.short_spec_name, nuc.A, nuc.Z))
+
+        # write out some network properties
+        with open("NETWORK_PROPERTIES", "w") as of:
+            of.write("NSCREEN {}".format(self.num_screen_calls))
+
+
