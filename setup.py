240a8fd3acbe1b4c3bfe7422cd68a41f53dca29d
Michael Zingale <michael.zingale@stonybrook.edu>
2021-01-28 15:08:07 -0500

    update the version number to 1.5

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='pynucastro',
-      version='1.4.1',
+      version='1.5.0',
       description='Python Interfaces to the nuclear reaction rate databases',
       url='https://github.com/pynucastro/nucastro',
-      author='Mike Zingale and Donald Willcox',
+      author='pynucastro development group'
       author_email='michael.zingale@stonybrook.edu',
       license='BSD',
       packages=find_packages(),
