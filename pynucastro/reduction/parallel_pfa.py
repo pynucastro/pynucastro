1921b9965813bc7fbf7dac6f2c061230f9e783d6
Luis Rangel DaCosta <luisrd@berkeley.edu>
2021-04-28 17:38:35 -0700

    add some output

--- pynucastro/reduction/parallel_pfa.py
+++ pynucastro/reduction/parallel_pfa.py
@@ -10,9 +10,14 @@ from mpi4py import MPI
 from path_flux_analysis import calc_adj_matrix, graph_from_adj_matrix, get_remove_list, get_reduced_network
 from generate_data import dataset as get_conditions
 
-def main(endpoint, targets =[Nucleus("p")], n=10, tol=0.2):
+def main(endpoint, targets =[Nucleus("p")], n=5, tol=0.2):
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    N_proc = comm.Get_size()
+
     net = load_network(endpoint)
-    print("Network loaded")
+    print("Network loaded on process %i" % rank)
+    sys.stdout.flush()
 
     conds = get_conditions(net, n)
     if isinstance(n, int):
@@ -20,21 +25,20 @@ def main(endpoint, targets =[Nucleus("p")], n=10, tol=0.2):
     else:
         n = np.array(list(n), dtype=np.int32)
 
-    comm = MPI.COMM_WORLD
-    rank = comm.Get_rank()
-    N_proc = comm.Get_size()
-
-
     rho_L = conds[0]
     T_L = conds[1]
     comp_L = conds[2]
     first_A = True
+    n_conds = np.prod(n)
     for i, rho in enumerate(rho_L):
         for j, T in enumerate(T_L):
             for k,comp in enumerate(comp_L):
                 iter = i*n[1]*n[2] + j*n[2] + k 
                 if iter % N_proc == rank:
                     rvals = net.evaluate_rates(rho=rho, T=T, composition=comp)
+                    if((iter % n_conds//10)):
+                        print("On condition %i of %i" % (iter, n_conds))
+                        sys.stdout.flush()
 
                     # grab adjacency matrix through PFA calculation on 2-neighbor paths
                     A = calc_adj_matrix(net, rvals, tol)
@@ -48,6 +52,9 @@ def main(endpoint, targets =[Nucleus("p")], n=10, tol=0.2):
         A_final = np.zeros(A.shape)
 
     comm.Barrier()
+    if(rank == 0):
+        print("Reducing adjacency matrix across processes")
+        sys.stdout.flush()
     comm.Reduce([A_red, MPI.DOUBLE], [A_final, MPI.DOUBLE], op=MPI.MAX, root=0)
 
     ## on rank 0 only
