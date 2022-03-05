22734ac35bd68452bec83a1e580eb97bc95cc873
Alexander Smith <soulalex@gmail.com>
2022-03-05 13:06:09 -0500

    Adding a ReacLibLibrary class which only reads the, 20180319default2 ReacLib snapshot, by default

--- pynucastro/rates/__init__.py
+++ pynucastro/rates/__init__.py
@@ -2,4 +2,4 @@
 
 __all__ = ["rate"]
 
-from .rate import Tfactors, Library, Rate, RateFilter, UnsupportedNucleus, Nucleus, list_known_rates
+from .rate import Tfactors, Library, ReacLibLibrary, Rate, RateFilter, UnsupportedNucleus, Nucleus, list_known_rates
