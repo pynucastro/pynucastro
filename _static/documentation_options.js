3cb02f840b14cee3333ce2ffc9d8a91f3269ea83
biboyd <biboyd@users.noreply.github.com>
2022-05-23 20:01:16 +0000

    deploy: 0a950b274a2f9c4186f924b4a9341c1a88f9c2a6

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.6.0.post105+g84451ac',
+    VERSION: '1.6.0.post106+g0a950b2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
