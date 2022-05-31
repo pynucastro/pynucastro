583724cfa501788be00bc1560873ada5b9d85165
Brendan Boyd <boyd.brendan@stonybrook.edu>
2022-05-31 17:05:13 -0400

    fix flake errors

--- pynucastro/networks/tests/test_rate_collection.py
+++ pynucastro/networks/tests/test_rate_collection.py
@@ -36,11 +36,10 @@ class TestRateCollection:
         r = self.rc.get_rate("he4_he4_he4__c12")
         assert r.fname == "he4_he4_he4__c12"
 
-
     def test_find_reverse(self):
         rr = self.rc.find_reverse(self.rc.get_rate("he4_c12__o16"))
         assert rr.fname == "o16__he4_c12"
-        
+
     def test_evaluate_energy_gen(self):
         # define a composition
         comp = pyna.Composition(self.rc.unique_nuclei)
@@ -51,7 +50,3 @@ class TestRateCollection:
         rho = 1e5
         T = 1e8
         assert self.rc.evaluate_energy_generation(rho, T, comp) == 32.24796008826701
-
-        
-
-        
