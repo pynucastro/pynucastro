d7dcf1ad8e9e96b119e41e6c73e34622c51f5fd9
Eric Johnson <yut23@users.noreply.github.com>
2022-09-09 17:58:07 -0400

    Add test to exercise writing a full python network (#390)

---
+++ pynucastro/networks/tests/test_full_python_net.py
@@ -0,0 +1,32 @@
+from pynucastro import networks
+import os
+import pytest
+
+
+class TestFullPythonNetwork:
+    @pytest.fixture(scope="class")
+    def fn(self):
+        files = ["c12-c12a-ne20-cf88",
+                 "c12-c12n-mg23-cf88",
+                 "c12-c12p-na23-cf88",
+                 "c12-ag-o16-nac2",
+                 "n--p-wc12",
+                 "he4-aag-c12-fy05"]
+
+        return networks.PythonNetwork(files)
+
+    def test_write_network(self, fn):
+        """test the write_network function"""
+        test_path = "_test_python/"
+        reference_path = "_python_reference/"
+        base_path = os.path.relpath(os.path.dirname(__file__))
+
+        test_file = "network.py"
+
+        os.makedirs(test_path, exist_ok=True)
+        fn.write_network(outfile=os.path.join(test_path, test_file))
+
+        # compare contents of files
+        with open(os.path.join(test_path, test_file), "r") as generated, \
+             open(os.path.join(base_path, reference_path, test_file), "r") as reference:
+            assert generated.readlines() == reference.readlines()
