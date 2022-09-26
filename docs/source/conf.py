4d90000e0c3d72c74ecb7090ee28040eb3146813
Michael Zingale <michael.zingale@stonybrook.edu>
2022-09-26 13:13:20 -0400

    execute notebooks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,9 @@ extensions = ['sphinx.ext.autodoc',
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# always execute notebooks
+nbsphinx_execute = 'always'
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
