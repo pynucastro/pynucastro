022d1774a7b2c11d054f12d522f89bdc4b422b96
Michael Zingale <michael.zingale@stonybrook.edu>
2022-10-07 14:22:20 -0400

    add unit tests

--- pynucastro/networks/tests/conftest.py
+++ pynucastro/networks/tests/conftest.py
@@ -5,3 +5,7 @@ import pytest
 @pytest.fixture(scope="package")
 def reaclib_library():
     return pyna.ReacLibLibrary()
+
+@pytest.fixture(scope="package")
+def tabular_library():
+    return pyna.TabularLibrary()
