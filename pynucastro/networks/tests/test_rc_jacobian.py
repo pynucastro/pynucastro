acc136c8ecd107bb0f049ff1542d9237f4115ada
Michael Zingale <michael.zingale@stonybrook.edu>
2022-10-23 18:21:58 -0400

    more tests

--- pynucastro/networks/tests/test_rc_jacobian.py
+++ pynucastro/networks/tests/test_rc_jacobian.py
@@ -20,16 +20,25 @@ class TestRateCollectionJacobian:
     def test_jac(self, rc, comp):
         rho = 1.e6
         T = 5.e8
+        ymolar = comp.get_molar()
+
         jac = rc.evaluate_jacobian(rho, T, comp)
 
         # let's look now at jac(0, 0), which should be dYdot(He4)/dY(He4)
         # there are 4 terms that go into this, which we will compute manually
 
+        jac_alpha_alpha = 0.0
+
         # 3-alpha
+        # r = rho**2 Y(alpha)**3 N_A<sigma v> / 6
+        # 3 alphas are destroyed
+        jac_alpha_alpha += -3.0 * rho**2 * 3 * ymolar[Nucleus("he4")]**2 
 
         # C12(a,g)O16
+        # r = rho * Y(alpha) * Y(c12) N_A<sigma v>
 
         # C12(C12,a)Ne20
+        # r = rho * Y(c12)**2 N_A<sigma v> / 2
 
         # O16(a,g)Ne20
-
+        # r = rho * Y(o16) * Y(alpha) N_A<sigma v>
