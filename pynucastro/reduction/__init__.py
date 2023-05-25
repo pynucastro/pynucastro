51f389cf7d3c01ea21e5da4e56d6935601c7f1a8
Kiran Eiden <kiraneiden@gmail.com>
2023-05-24 20:07:51 -0700

    Add sensitivity analysis functions to __init__.py

--- pynucastro/reduction/__init__.py
+++ pynucastro/reduction/__init__.py
@@ -1,3 +1,4 @@
 """Routines for nuclear reaction network reduction."""
 
+from .sensitivity_analysis import binary_search_trim, sens_analysis
 from .reduction_utils import mpi_importer, FailedMPIImport, mpi_numpy_decomp
