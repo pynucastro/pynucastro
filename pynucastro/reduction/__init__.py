ac77a96f641e83fb63f5963d21f029333eb7d284
Eric T. Johnson <eric.t.johnson@stonybrook.edu>
2023-06-29 17:39:25 -0400

    Fix linter warnings

--- pynucastro/reduction/__init__.py
+++ pynucastro/reduction/__init__.py
@@ -1,4 +1,4 @@
 """Routines for nuclear reaction network reduction."""
 
-from .sensitivity_analysis import binary_search_trim, sens_analysis
 from .reduction_utils import FailedMPIImport, mpi_importer, mpi_numpy_decomp
+from .sensitivity_analysis import binary_search_trim, sens_analysis
