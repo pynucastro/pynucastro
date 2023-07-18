9c9d9575da95383f9f554521b225002bbe88d289
zingale <zingale@users.noreply.github.com>
2023-07-18 12:00:22 +0000

    deploy: 102cacbbcc49d5ec86ad2ebdcdeaa9900d32704b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0.3.post61+g4fd4bbff',
+    VERSION: '2.0.3.post60+g102cacbb',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
