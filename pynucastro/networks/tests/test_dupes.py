772d0c256f50ed9a9b71b882028bd1255357fce1
Michael Zingale <michael.zingale@stonybrook.edu>
2023-08-15 11:10:57 -0400

    add a RateCollection method to find duplicate links we might have 2 rates from different sources that do the same thing.  Now we can do rc.find_duplicate_links()

---
+++ pynucastro/networks/tests/test_dupes.py
@@ -0,0 +1,35 @@
+# unit tests for rates
+import io
+
+import pytest
+
+import pynucastro as pyna
+
+
+class TestDuplicates:
+
+    @pytest.fixture(scope="class")
+    def ecsn_rc(self, reaclib_library):
+        all_nuclei = ["p", "he4",
+                      "ne20", "o20", "f20",
+                      "mg24", "al27", "o16",
+                      "si28", "s32", "p31"]
+        ecsn_rl_lib = reaclib_library.linking_nuclei(all_nuclei, with_reverse=True)
+
+        tabular_lib = pyna.TabularLibrary()
+        ecsn_tabular_lib = tabular_lib.linking_nuclei(["f20", "o20", "ne20"])
+
+        return pyna.RateCollection(libraries=[ecsn_rl_lib, ecsn_tabular_lib])
+
+    def test_validate(self, ecsn_rc):
+        dupes = ecsn_rc.find_duplicate_links()
+
+        assert len(dupes) == 2
+
+        d1 = dupes[0][0]
+        assert d1.reactants == [pyna.Nucleus("o20")]
+        assert d1.products == [pyna.Nucleus("f20")]
+
+        d2 = dupes[1][0]
+        assert d2.reactants == [pyna.Nucleus("f20")]
+        assert d2.products == [pyna.Nucleus("ne20")]
