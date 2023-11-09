ad88d5799606d91e4255a8ed1c90d01d5046e43b
Michael Zingale <michael.zingale@stonybrook.edu>
2023-11-09 12:51:29 -0500

    more conversions

--- pynucastro/constants/constants.py
+++ pynucastro/constants/constants.py
@@ -8,6 +8,7 @@ k_MeV = physical_constants.value("Boltzmann constant in eV/K") / physical_consta
 h = physical_constants.value("Planck constant") / physical_constants.erg
 hbar = physical_constants.value("reduced Planck constant") / physical_constants.erg
 N_A = physical_constants.value("Avogadro constant")
+q_e = physical_constants.value("elementary charge") * (physical_constants.c * 100) / 10  # C to statC (esu)
 
 erg2MeV = physical_constants.erg / (physical_constants.eV * physical_constants.mega)
 MeV2erg = (physical_constants.eV * physical_constants.mega) / physical_constants.erg
