5f30950ad3b0b922c831d1ef91259ec23e7f48ec
Eric T. Johnson <eric.t.johnson@stonybrook.edu>
2023-11-10 13:55:05 -0500

    Simplify and speed up duplicate rate checking
    This speeds up the sensitivity analysis example code by ~10-15%.

--- pynucastro/rates/known_duplicates.py
+++ pynucastro/rates/known_duplicates.py
@@ -1,8 +1,10 @@
+import collections
+
 # there are some exceptions to the no-duplicate rates restriction.  We
 # list them here by class name and then fname
 ALLOWED_DUPLICATES = [
-    (("ReacLibRate: p_p__d__weak__bet_pos_"),
-     ("ReacLibRate: p_p__d__weak__electron_capture"))
+    {"ReacLibRate: p_p__d__weak__bet_pos_",
+     "ReacLibRate: p_p__d__weak__electron_capture"}
 ]
 
 
@@ -10,23 +12,12 @@ def find_duplicate_rates(rate_list):
     """given a list of rates, return a list of groups of duplicate
     rates"""
 
-    duplicates = []
+    lookup = collections.defaultdict(list)
     for rate in rate_list:
-        same_links = [q for q in rate_list
-                      if q != rate and
-                      sorted(q.reactants) == sorted(rate.reactants) and
-                      sorted(q.products) == sorted(rate.products)]
+        lookup[tuple(sorted(rate.reactants)),
+               tuple(sorted(rate.products))].append(rate)
 
-        if same_links:
-            new_entry = [rate] + same_links
-            already_found = False
-            # we may have already found this pair
-            for dupe in duplicates:
-                if new_entry[0] in dupe:
-                    already_found = True
-                    break
-            if not already_found:
-                duplicates.append(new_entry)
+    duplicates = [entry for entry in lookup.values() if len(entry) > 1]
 
     return duplicates
 
@@ -35,17 +26,5 @@ def is_allowed_dupe(rate_list):
     """rate_list is a list of rates that provide the same connection
     in a network.  Return True if this is an allowed duplicate"""
 
-    for allowed_dupe in ALLOWED_DUPLICATES:
-        found = 0
-        if len(rate_list) == len(allowed_dupe):
-            found = 1
-            for r in rate_list:
-                rate_key = f"{r.__class__.__name__}: {r.fname}"
-                if rate_key in allowed_dupe:
-                    found *= 1
-                else:
-                    found *= 0
-            if found:
-                return True
-
-    return False
+    key_set = {f"{r.__class__.__name__}: {r.fname}" for r in rate_list}
+    return key_set in ALLOWED_DUPLICATES
