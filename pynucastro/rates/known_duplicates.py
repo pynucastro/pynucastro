d35968a4deb69c6306e38101d946c285935c26dc
Eric T. Johnson <eric.t.johnson@stonybrook.edu>
2023-11-10 14:24:49 -0500

    Add more comments

--- pynucastro/rates/known_duplicates.py
+++ pynucastro/rates/known_duplicates.py
@@ -9,15 +9,17 @@ ALLOWED_DUPLICATES = [
 
 
 def find_duplicate_rates(rate_list):
-    """given a list of rates, return a list of groups of duplicate
-    rates"""
+    """given a list of rates, return a list of groups of duplicate rates"""
 
-    lookup = collections.defaultdict(list)
+    # Group the rates into lists of potential duplicates, keyed by their
+    # reactants and products.
+    grouped_rates = collections.defaultdict(list)
     for rate in rate_list:
-        lookup[tuple(sorted(rate.reactants)),
-               tuple(sorted(rate.products))].append(rate)
+        grouped_rates[tuple(sorted(rate.reactants)),
+                      tuple(sorted(rate.products))].append(rate)
 
-    duplicates = [entry for entry in lookup.values() if len(entry) > 1]
+    # any entry in grouped_rates containing more than one rate is a duplicate
+    duplicates = [entry for entry in grouped_rates.values() if len(entry) > 1]
 
     return duplicates
 
@@ -26,5 +28,7 @@ def is_allowed_dupe(rate_list):
     """rate_list is a list of rates that provide the same connection
     in a network.  Return True if this is an allowed duplicate"""
 
+    # make rate_list into a set of strings in the same format as
+    # ALLOWED_DUPLICATES, then check if it matches any of the allowed sets
     key_set = {f"{r.__class__.__name__}: {r.fname}" for r in rate_list}
     return key_set in ALLOWED_DUPLICATES
