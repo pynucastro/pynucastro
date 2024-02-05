d6600d7018cb3667fee4ce19c40bda5466ce7774
Zhi <zhichen0428@gmail.com>
2024-02-05 15:54:12 -0500

    make _Decorators and add pylint exceptions

--- pynucastro/networks/rate_collection.py
+++ pynucastro/networks/rate_collection.py
@@ -421,13 +421,14 @@ class RateCollection:
 
         self._build_collection()
 
-    class Decorators:
+    class _Decorators:
         @classmethod
-        def _modifies_rates(cls, func):
+        def modifies_rates(cls, func):
             # a decorator that calls _build_collection after the func
             @functools.wraps(func)
             def wrapper(self, *args, **kwargs):
                 func(self, *args, **kwargs)
+                # pylint: disable-next=protected-access
                 self._build_collection()
             return wrapper
 
@@ -711,7 +712,7 @@ class RateCollection:
 
         return temp_arrays, temp_indices
 
-    @Decorators._modifies_rates
+    @_Decorators.modifies_rates
     def remove_nuclei(self, nuc_list):
         """remove the nuclei in nuc_list from the network along with any rates
         that directly involve them (this doesn't affect approximate rates that
@@ -728,7 +729,7 @@ class RateCollection:
         for rate in set(rates_to_delete):
             self.rates.remove(rate)
 
-    @Decorators._modifies_rates
+    @_Decorators.modifies_rates
     def remove_rates(self, rates):
         """remove the Rate objects in rates from the network.  Note, if
         rate list is a dict, then the keys are assumed to be the rates
@@ -740,7 +741,7 @@ class RateCollection:
             for r in rates:
                 self.rates.remove(r)
 
-    @Decorators._modifies_rates
+    @_Decorators.modifies_rates
     def add_rates(self, rates):
         """add the Rate objects in rates from the network."""
 
@@ -753,7 +754,7 @@ class RateCollection:
                 if r not in self.rates:
                     self.rates.append(r)
 
-    @Decorators._modifies_rates
+    @_Decorators.modifies_rates
     def make_ap_pg_approx(self, intermediate_nuclei=None):
         """combine the rates A(a,g)B and A(a,p)X(p,g)B (and the reverse) into a single
         effective approximate rate."""
