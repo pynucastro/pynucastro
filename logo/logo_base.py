925271bf2a96890405db6fe66da634ff87a85c10
Michael Zingale <michael.zingale@stonybrook.edu>
2024-06-04 15:07:13 -0400

    tweak sizes

--- logo/logo_base.py
+++ logo/logo_base.py
@@ -26,7 +26,7 @@ comp = pyna.Composition(rc.get_nuclei())
 comp.set_solar_like()
 
 fig = rc.plot(rho=2.e6, T=3.e7, comp=comp,
-              node_size=1300, node_font_size=16, node_color="#444444")
+              node_size=1300, node_font_size=18, node_color="#444444")
 
 # ax[0] is the plot, ax[1] is the colorbar
 ax = fig.get_axes()
@@ -38,13 +38,14 @@ print(ax)
 ax[0].set_axis_off()
 ax[1].remove()
 
-fig.text(0.4, 0.1, "pynucastro", horizontalalignment="center",
+fig.text(0.42, 0.1, "pynucastro", horizontalalignment="center",
          color="#444444",
          fontsize="40", fontstyle="italic", fontfamily="Fira Sans")
 
 ax[0].set_aspect("equal", adjustable="datalim")
 
-fig.set_size_inches(5, 4.5)
+fig.set_size_inches(4.5, 4.0)
+
 
 ax[0].margins(0.0)
 fig.tight_layout()
