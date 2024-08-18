34eb2878afeca01fb4121ae9d97275c77c11ece8
Michael Zingale <michael.zingale@stonybrook.edu>
2024-08-18 18:47:01 -0400

    fix isort

--- pynucastro/rates/__init__.py
+++ pynucastro/rates/__init__.py
@@ -5,7 +5,6 @@ __all__ = ["rate", "library"]
 from .known_duplicates import find_duplicate_rates, is_allowed_dupe
 from .library import (LangankeLibrary, Library, RateFilter, ReacLibLibrary,
                       SuzukiLibrary, TabularLibrary, list_known_rates)
-from .rate import (ApproximateRate, DerivedRate, Rate, RateFileError,
-                   RatePair, ReacLibRate, SingleSet, TableIndex,
-                   TableInterpolator, TabularRate, Tfactors,
-                   _find_rate_file, load_rate)
+from .rate import (ApproximateRate, DerivedRate, Rate, RateFileError, RatePair,
+                   ReacLibRate, SingleSet, TableIndex, TableInterpolator,
+                   TabularRate, Tfactors, _find_rate_file, load_rate)
