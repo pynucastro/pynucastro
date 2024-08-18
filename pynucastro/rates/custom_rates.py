9101785cce2dccc0f8c83a286232a054e41a3ec0
Michael Zingale <michael.zingale@stonybrook.edu>
2024-08-18 18:38:57 -0400

    add a custom rate for DeBoer 2017

---
+++ pynucastro/rates/custom_rates.py
@@ -0,0 +1,33 @@
+from pynucastro.nucdata import Nucleus
+from pynucastro.rates import ReacLibRate, SingleSet
+
+
+class DeBoerC12agO16(ReacLibRate):
+
+    def __init__(self):
+
+        labelprops = "deboer"
+
+        reactants = [Nucleus("c12"), Nucleus("he4")]
+        products = [Nucleus("o16")]
+
+        Q = 0
+        for n in reactants:
+            Q += -n.A * n.nucbind
+        for n in products:
+            Q += n.A * n.nucbind
+
+        super().__init__(reactants=reactants, products=products, Q=Q,
+                         labelprops=labelprops)
+
+        # from DeBoer et al. 2017
+        # https://arxiv.org/pdf/1709.03144
+        # table XXVI
+
+        # non-resonant
+        a = [24.1, 0.0, -32.0, -5.9, 1.8, -0.17, -2./3.]
+        self.sets.append(SingleSet(a, labelprops=labelprops))
+
+        # resonance
+        a = [7.4, -30.0, 0.0, 0.0, 0.0, 0.0, -1.5]
+        self.sets.append(SingleSet(a, labelprops=labelprops))
