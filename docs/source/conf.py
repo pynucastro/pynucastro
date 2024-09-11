c53920ecef5ea8b656bee0226c06d2792f09966d
Michael Zingale <michael.zingale@stonybrook.edu>
2024-09-11 15:31:24 -0400

    allow errors

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,6 +57,7 @@ templates_path = ['_templates']
 
 # always execute notebooks
 nbsphinx_execute = 'always'
+nbsphinx_allow_errprs = True
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
