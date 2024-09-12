74b367edc473bcb7936c5331e3304cd9f8fcd741
Michael Zingale <michael.zingale@stonybrook.edu>
2024-09-12 15:20:42 -0400

    add a notebook to the docs showing how to deal with duplicate rates (#777)
    it turns out that we had some examples about duplicate rates in the validate page, but it wasn't being listed in the TOC. This is now a separate page with a more descriptive example.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,7 @@ templates_path = ['_templates']
 
 # always execute notebooks
 nbsphinx_execute = 'always'
+nbsphinx_allow_errors = True
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
