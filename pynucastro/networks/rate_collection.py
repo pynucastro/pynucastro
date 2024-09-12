41011949fee7e6e70133fc38c1c715fa9b86e5c8
biboyd <boyd.brendan@stonybrook.edu>
2024-09-12 11:43:17 -0400

    add warning about n-decay rate

--- pynucastro/networks/rate_collection.py
+++ pynucastro/networks/rate_collection.py
@@ -568,6 +568,8 @@ class RateCollection:
             elif isinstance(r.chapter, int):
                 if r not in self.reaclib_rates:
                     self.reaclib_rates.append(r)
+                    if r.get_rate_id() == "n --> p <wc12_reaclib_weak_>":
+                        warnings.warn("ReacLib neutron decay rate (<n_to_p_weak_wc12>) does not account for degeneracy at high densities. Consider using tabular rate from Langanke.")
             else:
                 raise NotImplementedError(f"Chapter type unknown for rate chapter {r.chapter}")
 
