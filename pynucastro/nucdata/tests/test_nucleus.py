a3402e6517be7d7edc122a939eb413bb44c86500
SamG-01 <103982805+SamG-01@users.noreply.github.com>
2024-11-02 17:13:53 -0400

    fix test

--- pynucastro/nucdata/tests/test_nucleus.py
+++ pynucastro/nucdata/tests/test_nucleus.py
@@ -161,5 +161,5 @@ class TestNucleus:
         assert self.he4 == Nucleus.from_Z_A(2, 4)
 
     def test_add_subtract(self):
-        assert self.c12 + self.p == Nucleus("c13")
+        assert self.c12 + self.n == Nucleus("c13")
         assert self.d - self.n == self.h1
