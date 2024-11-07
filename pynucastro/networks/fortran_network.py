8e81d1fd23a056662dc3a804c9cb5642bf3bb22f
Michael Zingale <michael.zingale@stonybrook.edu>
2024-11-07 13:37:04 -0500

    fix nspec

--- pynucastro/networks/fortran_network.py
+++ pynucastro/networks/fortran_network.py
@@ -10,8 +10,13 @@ class FortranNetwork(SimpleCxxNetwork):
         # Initialize SimpleCxxNetwork parent class
         super().__init__(*args, **kwargs)
 
+        self.ftags['<nspec_fortran>'] = self._nspec_fortran
+
     def _get_template_files(self):
 
         path = self.pynucastro_dir/"templates/fortran-network"
 
         return path.glob("*.template")
+
+    def _nspec_fortran(self, n_indent, of):
+        of.write(f'{self.indent*n_indent}integer, parameter :: nspec = {len(self.unique_nuclei)}\n')
