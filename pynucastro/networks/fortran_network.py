a4fd2b60e596a3e2e7d426cf7d89da671abcd685
Michael Zingale <michael.zingale@stonybrook.edu>
2024-11-07 18:50:24 -0500

    add the nuclei properties to the Fortran module

--- pynucastro/networks/fortran_network.py
+++ pynucastro/networks/fortran_network.py
@@ -11,6 +11,9 @@ class FortranNetwork(SimpleCxxNetwork):
         super().__init__(*args, **kwargs)
 
         self.ftags['<nspec_fortran>'] = self._nspec_fortran
+        self.ftags['<aion_fortran>'] = self._aion_fortran
+        self.ftags['<zion_fortran>'] = self._zion_fortran
+        self.ftags['<name_fortran>'] = self._name_fortran
 
     def _get_template_files(self):
 
@@ -20,3 +23,47 @@ class FortranNetwork(SimpleCxxNetwork):
 
     def _nspec_fortran(self, n_indent, of):
         of.write(f'{self.indent*n_indent}integer, parameter :: nspec = {len(self.unique_nuclei)}\n')
+
+    def _aion_fortran(self, n_indent, of):
+        of.write(f'{self.indent*n_indent}double precision, parameter :: aion(nspec) = [ &\n')
+        for n, nuc in enumerate(self.unique_nuclei):
+            if n % 4 == 0:
+                of.write('        ')
+            of.write(f'{nuc.A}')
+            if n < len(self.unique_nuclei)-1:
+                of.write(', ')
+            if n % 4 == 3:
+                of.write(' &\n')
+        if len(self.unique_nuclei) % 4 == 0:
+            of.write('       ')
+        of.write(']\n')
+
+    def _zion_fortran(self, n_indent, of):
+        of.write(f'{self.indent*n_indent}double precision, parameter :: zion(nspec) = [ &\n')
+        for n, nuc in enumerate(self.unique_nuclei):
+            if n % 4 == 0:
+                of.write('        ')
+            of.write(f'{nuc.Z}')
+            if n < len(self.unique_nuclei)-1:
+                of.write(', ')
+            if n % 4 == 3:
+                of.write(' &\n')
+        if len(self.unique_nuclei) % 4 == 0:
+            of.write('       ')
+        of.write(']\n')
+
+    def _name_fortran(self, n_indent, of):
+        of.write(f'{self.indent*n_indent}character(len=6), dimension(nspec) :: spec_names = [ &\n')
+        for n, nuc in enumerate(self.unique_nuclei):
+            if n == 0:
+                of.write('        character(len=6) :: ')
+            elif n % 4 == 0:
+                of.write('                            ')
+            of.write(f'"{nuc.short_spec_name}"')
+            if n < len(self.unique_nuclei)-1:
+                of.write(', ')
+            if n % 4 == 3:
+                of.write(' &\n')
+        if len(self.unique_nuclei) % 4 == 0:
+            of.write('       ')
+        of.write(']\n')
