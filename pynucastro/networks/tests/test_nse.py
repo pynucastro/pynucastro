d9986743f63ece2143df89decfe44dcd0c636d50
Michael Zingale <michael.zingale@stonybrook.edu>
2025-01-06 16:09:38 -0500

    more updates

--- pynucastro/networks/tests/test_nse.py
+++ pynucastro/networks/tests/test_nse.py
@@ -23,7 +23,7 @@ class TestNSE:
         xsum = sum(nse_Xs)
 
         assert xsum == pytest.approx(1.0, rel=1.0e-10)
-        assert nse_Xs[0] == pytest.approx(0.009432340970989602, rel=1.0e-10)
+        assert nse_Xs[0] == pytest.approx(0.00943234095341063, rel=1.0e-10)
         assert nse_Xs[1] == pytest.approx(0.44083376851072315, rel=1.0e-10)
         assert nse_Xs[2] == pytest.approx(0.007140861274574405, rel=1.0e-10)
         assert nse_Xs[3] == pytest.approx(0.5187787534043939, rel=1.0e-10)
@@ -41,7 +41,7 @@ class TestNSE:
         xsum = sum(nse_Xs)
 
         assert xsum == pytest.approx(1.0, rel=1.0e-10)
-        assert nse_Xs[0] == pytest.approx(0.009096405176710266, rel=1.0e-10)
+        assert nse_Xs[0] == pytest.approx(0.009096405158020583, rel=1.0e-10)
         assert nse_Xs[1] == pytest.approx(0.46308446535589587, rel=1.0e-10)
         assert nse_Xs[2] == pytest.approx(0.006705879462385593, rel=1.0e-10)
         assert nse_Xs[3] == pytest.approx(0.5003022847190655, rel=1.0e-10)
