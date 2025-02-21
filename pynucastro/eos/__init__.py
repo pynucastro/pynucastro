85eb72eb9aa710f96a1fc6290f0e098d4595ed39
Alexander Smith <soulalex@gmail.com>
2025-02-20 23:18:21 -0500

    more isort

--- pynucastro/eos/__init__.py
+++ pynucastro/eos/__init__.py
@@ -2,5 +2,5 @@
 
 #__all__ = [fermi_integrals]
 
-from .fermi_integrals import fermi, dfermi_deta, dfermi_dbeta
 from .elec_pos_table import ElectronPositron
+from .fermi_integrals import dfermi_dbeta, dfermi_deta, fermi
