676013993945078302d45fe318f48957930801f4
Michael Zingale <michael.zingale@stonybrook.edu>
2025-05-23 10:21:01 -0400

    also fix some errors

--- pynucastro/nucdata/nucleus.py
+++ pynucastro/nucdata/nucleus.py
@@ -136,8 +136,8 @@ class Nucleus:
             self.pretty = r"\mathrm{p}_\mathrm{NSE}"
             self.caps_name = "p_NSE"
             self.nse = True
-        elif name.strip() in ("al-6", "al*6"):
-            raise UnsupportedNucleus()
+        elif name.lower().strip() in ("al-6", "al*6"):
+            raise UnsupportedNucleus("isomers of Al26 are not currently supported")
         else:
             if e := re.match(r"([a-zA-Z]+)(\d*)", name):
                 self.el = e.group(1).title()  # chemical symbol
