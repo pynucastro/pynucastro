9c714aa0d0c810598a82574570a2b8bd07c3262e
Michael Zingale <michael.zingale@stonybrook.edu>
2025-07-09 10:24:02 -0400

    tighten tols to 1.e-15

--- pynucastro/eos/tests/test_fd.py
+++ pynucastro/eos/tests/test_fd.py
@@ -17,12 +17,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(3.2968314946796124e-044, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(3.2968314946796139e-044, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(1.2363118105048548e-044, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(3.2968314946796129e-044, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(1.2363118105048556e-044, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-7.7269488156553455E-045, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(3.2968314946796124e-044, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(3.2968314946796139e-044, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(1.2363118105048548e-044, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(3.2968314946796129e-044, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(1.2363118105048556e-044, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-7.7269488156553455E-045, abs=1.e-100, rel=1.e-15)
 
         eta = -50
         beta = 1.0
@@ -30,12 +30,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(2.2314386397062104E-022, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(2.2314386397062108E-022, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(4.4513616188460852E-023, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(2.2314386397062099E-022, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(4.4513616188460870E-023, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-1.0696566775877673E-023, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(2.2314386397062104E-022, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(2.2314386397062108E-022, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(4.4513616188460852E-023, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(2.2314386397062099E-022, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(4.4513616188460870E-023, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-1.0696566775877673E-023, abs=1.e-100, rel=1.e-15)
 
         eta = 1.0
         beta = 10.0
@@ -43,12 +43,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(4.3093121530612724, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(3.0909917562756424, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(0.19002958762134178, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(1.6796585258434078, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(0.13977966489586524, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-8.4816696742261236E-003, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(4.3093121530612724, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(3.0909917562756424, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(0.19002958762134178, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(1.6796585258434078, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(0.13977966489586524, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-8.4816696742261236E-003, abs=1.e-100, rel=1.e-15)
 
         eta = 500.0
         beta = 100.0
@@ -56,12 +56,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(883930.45936891437, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(3535.6046159037460, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(4419.2988177917523, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(7.0710678132825819, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(17.677315986879442, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-22.094727366363866, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(883930.45936891437, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(3535.6046159037460, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(4419.2988177917523, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(7.0710678132825819, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(17.677315986879442, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-22.094727366363866, abs=1.e-100, rel=1.e-15)
 
     def test_k_m0_5(self):
         # compare k = -1/2 solutions to the Gong et al. code
@@ -74,12 +74,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(2.7816742731063666e-043, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(2.7816742731063678e-043, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(1.2653970717385436e-045, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(2.7816742731063674e-043, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(1.2653970717385444e-045, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-5.9528644489672306e-048, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(2.7816742731063666e-043, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(2.7816742731063678e-043, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(1.2653970717385436e-045, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(2.7816742731063674e-043, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(1.2653970717385444e-045, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-5.9528644489672306e-048, abs=1.e-100, rel=1.e-15)
 
         eta = -50
         beta = 0.0
@@ -87,12 +87,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(3.4186200954570750e-022, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(3.4186200954570741e-022, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(4.2732751193213414e-023, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(3.4186200954570736e-022, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(4.2732751193213450e-023, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-1.6024781697455028e-023, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(3.4186200954570750e-022, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(3.4186200954570741e-022, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(4.2732751193213414e-023, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(3.4186200954570736e-022, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(4.2732751193213450e-023, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-1.6024781697455028e-023, abs=1.e-100, rel=1.e-15)
 
         eta = 100.0
         beta = 100.0
@@ -100,12 +100,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(707.87776608227455, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(7.0717751162111329, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(3.5323860528718423, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(-7.0773544184610238E-006, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(3.5351802891431347E-002, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-1.7633986737239839E-002, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(707.87776608227455, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(7.0717751162111329, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(3.5323860528718423, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(-7.0773544184610238E-006, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(3.5351802891431347E-002, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-1.7633986737239839E-002, abs=1.e-100, rel=1.e-15)
 
     def test_k_1_5(self):
         # compare k = 3/2 solutions to the Gong et al. code
@@ -118,12 +118,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(1.2553979904636453E-032, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(1.2553979904636447E-032, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(5.7230338002235925E-034, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(1.2553979904636453E-032, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(5.7230338002235968E-034, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-2.6212332799199379E-035, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(1.2553979904636453E-032, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(1.2553979904636447E-032, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(5.7230338002235925E-034, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(1.2553979904636453E-032, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(5.7230338002235968E-034, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-2.6212332799199379E-035, abs=1.e-100, rel=1.e-15)
 
         eta = -20
         beta = 100.0
@@ -131,12 +131,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(2.9294159670904935E-008, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(2.9294159663320433E-008, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(1.4502712363335996E-010, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(2.9294159648151430E-008, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(1.4502712359617485E-010, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-7.1804917485691793E-013, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(2.9294159670904935E-008, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(2.9294159663320433E-008, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(1.4502712363335996E-010, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(2.9294159648151430E-008, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(1.4502712359617485E-010, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-7.1804917485691793E-013, abs=1.e-100, rel=1.e-15)
 
         eta = 40
         beta = 10000.0
@@ -144,12 +144,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(1517805.2872690351, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(113369.99663886119, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(75.889697517711070, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(5656.8613205601887, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(5.6684715477425227, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-3.7944565338813361E-003, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(1517805.2872690351, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(113369.99663886119, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(75.889697517711070, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(5656.8613205601887, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(5.6684715477425227, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-3.7944565338813361E-003, abs=1.e-100, rel=1.e-15)
 
     def test_k_2_5(self):
         # compare k = 5/2 solutions to the Gong et al. code
@@ -162,12 +162,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(2.1821367305243183E-023, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(2.1821367305243180E-023, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(6.9671437808138560E-025, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(2.1821367305243186E-023, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(6.9671437808138587E-025, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-2.2261771808312711E-026, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(2.1821367305243183E-023, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(2.1821367305243180E-023, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(6.9671437808138560E-025, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(2.1821367305243186E-023, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(6.9671437808138587E-025, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-2.2261771808312711E-026, abs=1.e-100, rel=1.e-15)
 
         eta = 10
         beta = 1.e-4
@@ -175,12 +175,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(1034.9073854351607, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(335.76592223107093, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(2231.0524130570820, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(80.071134602962601, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(905.09777777902468, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-5201.1741381184684, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(1034.9073854351607, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(335.76592223107093, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(2231.0524130570820, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(80.071134602962601, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(905.09777777902468, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-5201.1741381184684, abs=1.e-100, rel=1.e-15)
 
         eta = 100
         beta = 1.0
@@ -188,12 +188,12 @@ class TestFermiDirac:
         f = FermiIntegral(k, eta, beta)
         f.evaluate()
 
-        assert f.F == approx(17946771.869476113, abs=1.e-100, rel=1.e-13)
-        assert f.dF_deta == approx(714843.05556091876, abs=1.e-100, rel=1.e-13)
-        assert f.dF_dbeta == approx(8740888.8928929009, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_deta2 == approx(21361.250145668106, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_detadbeta == approx(350417.80107778130, abs=1.e-100, rel=1.e-13)
-        assert f.d2F_dbeta2 == approx(-4257540.4021439729, abs=1.e-100, rel=1.e-13)
+        assert f.F == approx(17946771.869476113, abs=1.e-100, rel=1.e-15)
+        assert f.dF_deta == approx(714843.05556091876, abs=1.e-100, rel=1.e-15)
+        assert f.dF_dbeta == approx(8740888.8928929009, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_deta2 == approx(21361.250145668106, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_detadbeta == approx(350417.80107778130, abs=1.e-100, rel=1.e-15)
+        assert f.d2F_dbeta2 == approx(-4257540.4021439729, abs=1.e-100, rel=1.e-15)
 
     def test_dfdeta(self):
 
