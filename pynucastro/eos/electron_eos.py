fa4b821a13e4751ea50f12662301b895ad597db6
Michael Zingale <michael.zingale@stonybrook.edu>
2025-07-17 16:38:42 -0400

    add positrons

--- pynucastro/eos/electron_eos.py
+++ pynucastro/eos/electron_eos.py
@@ -7,7 +7,7 @@ from pynucastro.constants import constants
 
 from .fermi_integrals import FermiIntegral
 
-EOSState = namedtuple("EOSState", ["p_e", "e_e", "eta"])
+EOSState = namedtuple("EOSState", ["p_e", "e_e", "p_pos", "e_pos", "eta"])
 
 
 class ElectronEOS:
@@ -54,23 +54,38 @@ class ElectronEOS:
         # our Fermi integrals will use a dimensionless temperature
         beta = constants.k * T / (constants.m_e * constants.c_light**2)
 
+        coeff = 8 * np.pi * np.sqrt(2) * (constants.m_e * constants.c_light / constants.h)**3 * beta**1.5
+
+        def n_e_fermi(eta):
+            f12 = FermiIntegral(0.5, eta, beta)
+            f12.evaluate(do_first_derivs=False, do_second_derivs=False)
+
+            f32 = FermiIntegral(1.5, eta, beta)
+            f32.evaluate(do_first_derivs=False, do_second_derivs=False)
+
+            return coeff * (f12.F + beta * f32.F)
+
+        def n_pos_fermi(eta):
+            eta_pos = -eta - 2.0/beta
+
+            f12 = FermiIntegral(0.5, eta_pos, beta)
+            f12.evaluate(do_first_derivs=False, do_second_derivs=False)
+
+            f32 = FermiIntegral(1.5, eta_pos, beta)
+            f32.evaluate(do_first_derivs=False, do_second_derivs=False)
+
+            return coeff * (f12.F + beta * f32.F)
+
         # compute the degeneracy parameter
         if self.include_positrons:
-            raise ValueError("include_positrons not yet implemented")
+            eta = brentq(lambda eta: n_e - (n_e_fermi(eta) - n_pos_fermi(eta)),
+                         eta_guess_min, eta_guess_max)
         else:
-            coeff = 8 * np.pi * np.sqrt(2) * (constants.m_e * constants.c_light / constants.h)**3 * beta**1.5
-
-            def n_e_fermi(eta):
-                f12 = FermiIntegral(0.5, eta, beta)
-                f12.evaluate(do_first_derivs=False, do_second_derivs=False)
-
-                f32 = FermiIntegral(1.5, eta, beta)
-                f32.evaluate(do_first_derivs=False, do_second_derivs=False)
-
-                return coeff * (f12.F + beta * f32.F)
-
             eta = brentq(lambda eta: n_e - n_e_fermi(eta),
                          eta_guess_min, eta_guess_max)
+
+        # for positrons
+        eta_pos = -eta - 2.0/beta
 
         # compute the pressure and energy
         pcoeff = coeff * (2.0 / 3.0) * constants.m_e * constants.c_light**2 * beta
@@ -85,4 +100,17 @@ class ElectronEOS:
         p_e = pcoeff * (f32.F + 0.5 * beta * f52.F)
         e_e = ecoeff * (f32.F + beta * f52.F) / rho
 
-        return EOSState(eta=eta, p_e=p_e, e_e=e_e)
+        p_pos = 0.0
+        e_pos = 0.0
+
+        if self.include_positrons:
+            f32_pos = FermiIntegral(1.5, eta_pos, beta)
+            f32_pos.evaluate(do_first_derivs=False, do_second_derivs=False)
+
+            f52_pos = FermiIntegral(2.5, eta_pos, beta)
+            f52_pos.evaluate(do_first_derivs=False, do_second_derivs=False)
+
+            p_pos = pcoeff * (f32_pos.F + 0.5 * beta * f52_pos.F)
+            e_pos = ecoeff * (f32_pos.F + beta * f52_pos.F) / rho
+
+        return EOSState(eta=eta, p_e=p_e, e_e=e_e, p_pos=p_pos, e_pos=e_pos)
