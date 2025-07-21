13bfc09d6a564149785dc6917f6217533b9def6b
Michael Zingale <michael.zingale@stonybrook.edu>
2025-07-21 14:29:36 -0400

    finish

--- pynucastro/eos/fermi_integrals.py
+++ pynucastro/eos/fermi_integrals.py
@@ -312,22 +312,17 @@ class FermiIntegral:
 
         N = len(x_leg)//2
 
-        integral = 0
-
         # We set the correspondence (a+b)/2 -> 0 and map the (-1,0) and (0,1)
         # intervals separately.
 
         fac1 = (a + b)/2
         fac2 = (b - a)/2
 
-        for root, weight in zip(x_leg[N:], w_leg[N:]):
-            x_1 = fac1 + fac2 * root
-            x_2 = fac1 - fac2 * root
-            qder_1 = kernel(x_1, k, eta, beta,
-                            eta_der=eta_der, beta_der=beta_der)
-            qder_2 = kernel(x_2, k, eta, beta,
-                            eta_der=eta_der, beta_der=beta_der)
-            integral += (qder_1 + qder_2) * weight
+        integral = sum((kernel(fac1 + fac2 * root, k, eta, beta,
+                               eta_der=eta_der, beta_der=beta_der) +
+                        kernel(fac1 - fac2 * root, k, eta, beta,
+                               eta_der=eta_der, beta_der=beta_der)) * weight
+                       for root, weight in zip(x_leg[N:], w_leg[N:]))
 
         integral *= (b - a) / 2
         return integral
