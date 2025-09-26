659e699c38c5a29a69b2a0a98c2b9045df0d91a7
Melissa Rasmussen <melissa.rasmussen@stonybrook.edu>
2025-09-26 12:51:47 -0400

    update spin table test to new interface

--- pynucastro/nucdata/tests/test_spin.py
+++ pynucastro/nucdata/tests/test_spin.py
@@ -26,19 +26,16 @@ class TestSpin:
         #However, due to some experimental uncertainties two values
         #are proposed.
 
-        assert self.spintable_gs.get_spin_states(a=1, z=0, reliable=True) == 2
-        assert self.spintable_gs.get_spin_states(a=14, z=4, reliable=True) == 1
-        assert self.spintable_gs.get_spin_states(a=219, z=89, reliable=True) == 10
-        with pytest.raises(NotImplementedError):
-            self.spintable_gs.get_spin_states(a=91, z=46, reliable=True)
-        with pytest.raises(NotImplementedError):
-            self.spintable_gs.get_spin_states(a=275, z=107, reliable=True)
-        with pytest.raises(NotImplementedError):
-            self.spintable_gs.get_spin_states(a=11, z=8, reliable=True)
+        assert self.spintable_gs.get_spin_states(a=1, z=0) == 2
+        assert self.spintable_gs.get_spin_states(a=14, z=4) == 1
+        assert self.spintable_gs.get_spin_states(a=219, z=89) == 10
+        assert self.spintable_gs.get_spin_states(a=91, z=46) == 8
+        assert self.spintable_gs.get_spin_states(a=275, z=107) == 6
+        assert self.spintable_gs.get_spin_states(a=11, z=8) == 4
 
-        assert self.spintable_gs.get_spin_states(a=1, z=0, reliable=False) == 2
-        assert self.spintable_gs.get_spin_states(a=14, z=4, reliable=False) == 1
-        assert self.spintable_gs.get_spin_states(a=219, z=89, reliable=False) == 10
-        assert self.spintable_gs.get_spin_states(a=91, z=46, reliable=False) == 8
-        assert self.spintable_gs.get_spin_states(a=275, z=107, reliable=False) == 6
-        assert self.spintable_gs.get_spin_states(a=11, z=8, reliable=False) == 4
+        assert self.spintable_gs.get_spin_reliability(a=1, z=0) is True
+        assert self.spintable_gs.get_spin_reliability(a=14, z=4) is True
+        assert self.spintable_gs.get_spin_reliability(a=219, z=89) is True
+        assert self.spintable_gs.get_spin_reliability(a=91, z=46) is False
+        assert self.spintable_gs.get_spin_reliability(a=275, z=107) is False
+        assert self.spintable_gs.get_spin_reliability(a=11, z=8) is False
