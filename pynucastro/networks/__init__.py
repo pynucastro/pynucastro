f7194d58eceaf24fdca070c96ce86cfa23fefc3b
khanakbhargava <khanak.bhargava@stonybrook.edu>
2025-11-30 20:27:26 -0500

    Makings suggested changes

--- pynucastro/networks/__init__.py
+++ pynucastro/networks/__init__.py
@@ -42,7 +42,7 @@ from .fortran_network import FortranNetwork
 from .helper import network_helper
 from .nse_network import NSENetwork
 from .python_network import PythonNetwork
-from .rate_collection import (Composition, Explorer, Lodders, RateCollection,
+from .rate_collection import (Composition, Explorer, LoddersComposition, RateCollection,
                               RateDuplicationError)
 from .simple_cxx_network import SimpleCxxNetwork
 from .sympy_network_support import SympyRates
--- pynucastro/networks/tests/test_lodders_comp.py
+++ pynucastro/networks/tests/test_lodders_comp.py
@@ -9,12 +9,12 @@ class TestLoddersComposition:
     @pytest.fixture(scope="class")
     def solar(self):
         """Default Lodders solar comp, without scaling"""
-        return networks.Lodders()
+        return networks.LoddersComposition()
 
     @pytest.fixture(scope="class")
     def scaled(self):
         """Scale Lodders to desired metallicity"""
-        return networks.Lodders(z=0.04)
+        return networks.LoddersComposition(z=0.04)
 
     def test_sum_to_one(self, solar, scaled):
         """All mass fractions should add up to 1"""
