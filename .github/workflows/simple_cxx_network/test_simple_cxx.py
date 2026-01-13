11e13d05e5f254a5c0b2822f78f41223b885ab37
zhi <zhichen0428@gmail.com>
2026-01-13 13:29:59 -0500

    add derived rate to simple cxx and fortran net compile tests

--- .github/workflows/simple_cxx_network/test_simple_cxx.py
+++ .github/workflows/simple_cxx_network/test_simple_cxx.py
@@ -1,5 +1,12 @@
 import pynucastro as pyna
 
 lib = pyna.ReacLibLibrary().linking_nuclei(["he4", "c12", "o16", "ne20", "na20"])
+rates_to_derive = lib.backward().get_rates()
+for r in rates_to_derive:
+    fr = lib.get_rate_by_nuclei(r.products, r.reactants)
+    if fr:
+        lib.remove_rate(r)
+        d = pyna.DerivedRate(fr, use_pf=False)
+        lib.add_rate(d)
 net = pyna.SimpleCxxNetwork(libraries=[lib])
 net.write_network()
