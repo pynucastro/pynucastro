50a1fa7254f765174b273c312714dc2637d48783
Michael Zingale <michael.zingale@stonybrook.edu>
2026-01-15 14:05:57 -0500

    read in the table

--- pynucastro/eos/tabular_electron_eos.py
+++ pynucastro/eos/tabular_electron_eos.py
@@ -12,6 +12,41 @@ from pynucastro.constants import constants
 from pynucastro.rates.files import _pynucastro_dir
 
 
+class Helmholtz:
+    """a container to hold the Helmholtz state and its derivatives"""
+
+    __slots__ = ("F", "dF_drho", "dF_dT",
+                 "d2F_drho2", "d2F_dT2", "d2F_drhodT",
+                 "d3F_drho2dT", "d3F_drhodT2", "d4F_drho2dT2")
+
+    def __init__(self,
+                 F=None, dF_drho=None, dF_dT=None,
+                 d2F_drho2=None, d2F_dT2=None, d2F_drhodT=None,
+                 d3F_drho2dT=None, d3F_drhodT2=None, d4F_drho2dT2=None):
+        self.F = float(F)
+        self.dF_drho = float(dF_drho)
+        self.dF_dT = float(dF_dT)
+        self.d2F_drho2 = float(d2F_drho2)
+        self.d2F_dT2 = float(d2F_dT2)
+        self.d2F_drhodT = float(d2F_drhodT)
+        self.d3F_drho2dT = float(d3F_drho2dT)
+        self.d3F_drhodT2 = float(d3F_drhodT2)
+        self.d4F_drho2dT2 = float(d4F_drho2dT2)
+
+
+class ThermoQuantity:
+    """a container to hold a thermodynamic quantity, its first
+    derivatives, and its 2nd crossed derivative."""
+
+    __slots__ = ("q", "dq_drho", "dq_dT", "d2q_drhodT")
+
+    def __init__(self, q=None, dq_drho=None, dq_dT=None, d2q_drhodT=None):
+        self.q = float(q)
+        self.dq_drho = float(dq_drho)
+        self.dq_dT = float(dq_dT)
+        self.d2q_drhodT = float(d2q_drhodT)
+
+
 class TabularElectronEOS:
     """tabular EOS"""
 
@@ -27,15 +62,60 @@ class TabularElectronEOS:
             while line := tf.readline():
                 if line.startswith("#"):
                     if g := rho_line.match(line):
-                        self.log10_rho_min = g.group(1)
-                        self.log10_rho_max = g.group(2)
-                        self.rho_npts = g.group(3)
+                        self.log10_rho_min = float(g.group(1))
+                        self.log10_rho_max = float(g.group(2))
+                        self.rho_npts = int(g.group(3))
                     elif g := temp_line.match(line):
-                        self.log10_temp_min = g.group(1)
-                        self.log10_temp_max = g.group(2)
-                        self.temp_npts = g.group(3)
+                        self.log10_temp_min = float(g.group(1))
+                        self.log10_temp_max = float(g.group(2))
+                        self.temp_npts = int(g.group(3))
                     elif g := version_line.match(line):
                         self.table_version = g.group(1)
+
+        # now read and store the data in 4 separate lists
+        with bz2.open(_pynucastro_dir / "eos" / self.table, mode="rt") as tf:
+
+            # the first rho_npts * temp_npts lines are the Helmholtz data
+            self.helm = []
+            for _ in range(self.rho_npts * self.temp_npts):
+                line = tf.readline()
+                fields = line.split()
+                h = Helmholtz(F=fields[0],
+                              dF_drho=fields[1], dF_dT=fields[2],
+                              d2F_drho2=fields[3], d2F_dT2=fields[4], d2F_drhodT=fields[5],
+                              d3F_drho2dT=fields[6], d3F_drhodT2=fields[7], d4F_drho2dT2=fields[8])
+                self.helm.append(h)
+
+            # the next rho_npts * temp_npts lines are pressure derivatives
+            self.dp_drho = []
+            for _ in range(self.rho_npts * self.temp_npts):
+                line = tf.readline()
+                fields = line.split()
+                q = ThermoQuantity(q=fields[0],
+                                   dq_drho=fields[1], dq_dT=fields[2],
+                                   d2q_drhodT=fields[3])
+                self.dp_drho.append(q)
+
+            # the next rho_npts * temp_npts lines are the degeneracy parameter
+            self.psi = []
+            for _ in range(self.rho_npts * self.temp_npts):
+                line = tf.readline()
+                fields = line.split()
+                q = ThermoQuantity(q=fields[0],
+                                   dq_drho=fields[1], dq_dT=fields[2],
+                                   d2q_drhodT=fields[3])
+                self.psi.append(q)
+
+            # the final rho_npts * temp_npts lines are the number density
+            self.ne = []
+            for _ in range(self.rho_npts * self.temp_npts):
+                line = tf.readline()
+                fields = line.split()
+                q = ThermoQuantity(q=fields[0],
+                                   dq_drho=fields[1], dq_dT=fields[2],
+                                   d2q_drhodT=fields[3])
+                self.ne.append(q)
+
 
     def __str__(self):
         ostr = ""
