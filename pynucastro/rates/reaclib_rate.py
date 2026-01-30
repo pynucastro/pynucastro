496969caa69102dda5032017721c6165f0a7e6d8
zhi <zhichen0428@gmail.com>
2026-01-30 15:57:22 -0500

    fix reaclib function string indent

--- pynucastro/rates/reaclib_rate.py
+++ pynucastro/rates/reaclib_rate.py
@@ -655,15 +655,14 @@ class ReacLibRate(Rate):
         fstring += f"    log_scor = rate_eval.{self.fname}\n\n"
         for s in self.sets:
             fstring += f"    # {s.labelprops[0:5]}\n"
-            set_string = s.set_string_py(prefix="ln_set_rate", plus_equal=True, with_exp=False)
+            set_string = s.set_string_py(prefix="ln_set_rate", plus_equal=False, with_exp=False)
             for t in set_string.split("\n"):
                 fstring += "    " + t + "\n"
-                fstring += "\n"
-                fstring += "    ln_set_rate += log_scor\n"
-                fstring += "    set_rate = np.exp(ln_set_rate)\n"
-                fstring += "    rate += set_rate\n\n"
+            fstring += "\n"
+            fstring += "    ln_set_rate += log_scor\n"
+            fstring += "    set_rate = np.exp(ln_set_rate)\n"
+            fstring += "    rate += set_rate\n\n"
 
-        fstring += "\n"
         fstring += f"    rate_eval.{self.fname} = rate\n\n"
         return fstring
 
