cd26ede35cacfb445724c10e9e15acc4d3298bfc
zhi <zhichen0428@gmail.com>
2026-03-12 17:04:54 -0400

    sync up

--- pynucastro/rates/tests/test_tabular_library.py
+++ pynucastro/rates/tests/test_tabular_library.py
@@ -11,19 +11,19 @@ class TestTabularLibrary:
     def test_number_of_rates(self, tl_default):
         assert tl_default.num_rates == 739
 
-        suzuki_rates = [r for r in tl_default.get_rates() if r.rfile.startswith("suzuki")]
+        suzuki_rates = [r for r in tl_default.get_rates() if r.rfile_name.startswith("suzuki")]
         assert len(suzuki_rates) == 61
 
-        langanke_rates = [r for r in tl_default.get_rates() if r.rfile.startswith("langanke")]
+        langanke_rates = [r for r in tl_default.get_rates() if r.rfile_name.startswith("langanke")]
         assert len(langanke_rates) == 212
 
-        ffn_rates = [r for r in tl_default.get_rates() if r.rfile.startswith("ffn")]
+        ffn_rates = [r for r in tl_default.get_rates() if r.rfile_name.startswith("ffn")]
         assert len(ffn_rates) == 88
 
-        oda_rates = [r for r in tl_default.get_rates() if r.rfile.startswith("oda")]
+        oda_rates = [r for r in tl_default.get_rates() if r.rfile_name.startswith("oda")]
         assert len(oda_rates) == 97
 
-        pruet_rates = [r for r in tl_default.get_rates() if r.rfile.startswith("pruet")]
+        pruet_rates = [r for r in tl_default.get_rates() if r.rfile_name.startswith("pruet")]
         assert len(pruet_rates) == 281
 
         # make sure the sum of all the different sources equals the
@@ -37,20 +37,20 @@ class TestTabularLibrary:
 
         assert tl_new.num_rates == 458
 
-        suzuki_rates = [r for r in tl_new.get_rates() if r.rfile.startswith("suzuki")]
+        suzuki_rates = [r for r in tl_new.get_rates() if r.rfile_name.startswith("suzuki")]
         assert len(suzuki_rates) == 0
 
-        langanke_rates = [r for r in tl_new.get_rates() if r.rfile.startswith("langanke")]
+        langanke_rates = [r for r in tl_new.get_rates() if r.rfile_name.startswith("langanke")]
         assert len(langanke_rates) == 62
 
-        ffn_rates = [r for r in tl_new.get_rates() if r.rfile.startswith("ffn")]
+        ffn_rates = [r for r in tl_new.get_rates() if r.rfile_name.startswith("ffn")]
         assert len(ffn_rates) == 376
 
-        oda_rates = [r for r in tl_new.get_rates() if r.rfile.startswith("oda")]
+        oda_rates = [r for r in tl_new.get_rates() if r.rfile_name.startswith("oda")]
         assert len(oda_rates) == 20
 
         # these are left out
-        pruet_rates = [r for r in tl_new.get_rates() if r.rfile.startswith("pruet")]
+        pruet_rates = [r for r in tl_new.get_rates() if r.rfile_name.startswith("pruet")]
         assert len(pruet_rates) == 0
 
         # make sure the sum of all the different sources equals the
