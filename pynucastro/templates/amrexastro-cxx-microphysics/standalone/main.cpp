c52b1d07d2da86c0c81d18827a6d421972e92764
Michael Zingale <michael.zingale@stonybrook.edu>
2026-04-12 15:40:04 -0400

    finish

--- pynucastro/templates/amrexastro-cxx-microphysics/standalone/main.cpp
+++ pynucastro/templates/amrexastro-cxx-microphysics/standalone/main.cpp
@@ -5,6 +5,7 @@
 #include <eos.H>
 #include <network.H>
 #include <unit_test.H>
+#include <actual_rhs.H>
 
 int main(int argc, char *argv[]) {
 
@@ -30,6 +31,22 @@ int main(int argc, char *argv[]) {
     for (int n = 0; n < NumSpec; ++n) {
         burn_state.xn[n] = 1.0 / NumSpec;
     }
+
+    amrex::Array1D<amrex::Real, 1, neqs> ydot{};
+
+    actual_rhs(burn_state, ydot);
+
+    std::cout << std::setprecision(12);
+
+    std::cout << "rho, T = " << burn_state.rho << " " << burn_state.T << std::endl;
+
+    for (int n = 1; n <= NumSpec; ++n) {
+        std::cout << "Ydot(" << short_spec_names_cxx[n-1] << ") = " << ydot(n) << std::endl;
+    }
+
+    std::cout << std::endl;
+
+
 
     amrex::Finalize();
 }
