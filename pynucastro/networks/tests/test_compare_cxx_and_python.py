81ba1a0fcaf2c852ba53321a9f0a5b35c6f4d2c1
Michael Zingale <michael.zingale@stonybrook.edu>
2026-04-13 08:04:36 -0400

    hook in the amrex test

--- pynucastro/networks/tests/test_compare_cxx_and_python.py
+++ pynucastro/networks/tests/test_compare_cxx_and_python.py
@@ -22,12 +22,15 @@ class TestNetworkCompare:
 
     @pytest.fixture(scope="class")
     def nc(self, lib):
-        test_path = Path("_test_compare/")
+        cxx_test_path = Path("_test_compare_cxx/")
+        amrex_test_path = Path("_test_compare_amrex/")
 
         nc = NetworkCompare(lib,
+                            include_amrex=True,
                             include_simple_cxx=True,
                             python_module_name="basic_cxx_py_compare.py",
-                            cxx_test_path=test_path)
+                            amrex_test_path=amrex_test_path,
+                            cxx_test_path=cxx_test_path)
         return nc
 
     @pytest.mark.skipif(sys.platform.startswith("win"), reason="Does not run on Windows")
@@ -45,18 +48,32 @@ class TestNetworkCompare:
             assert nc.ydots_cxx[nuc] == approx(nc.ydots_py_inline[nuc],
                                                rel=1.e-11, abs=1.e-14)
 
+        # compare the AMReX C++ net to the python inline version
+
+        for nuc in nc.ydots_amrex:
+            assert nc.ydots_amrex[nuc] == approx(nc.ydots_py_inline[nuc],
+                                                 rel=1.e-11, abs=1.e-14)
+
+        # compare the python module version to the python inline version
+
+        for nuc in nc.ydots_py_module:
+            assert nc.ydots_py_module[nuc] == approx(nc.ydots_py_inline[nuc],
+                                                     rel=1.e-11, abs=1.e-14)
+
+        # other comparisons -- these should not be needed, since we already
+        # compared everything to the python inline version
+
         # compare the simple C++ net to the python module version
 
         for nuc in nc.ydots_cxx:
             assert nc.ydots_cxx[nuc] == approx(nc.ydots_py_module[nuc],
                                                rel=1.e-11, abs=1.e-14)
 
-        # compare the python inline and module versions (shouldn't
-        # really be needed)
+        # compare the simple C++ net to the AMReX C++ net
 
-        for nuc in nc.ydots_py_inline:
-            assert nc.ydots_py_inline[nuc] == approx(nc.ydots_py_module[nuc],
-                                                     rel=1.e-11, abs=1.e-14)
+        for nuc in nc.ydots_cxx:
+            assert nc.ydots_cxx[nuc] == approx(nc.ydots_amrex[nuc],
+                                               rel=1.e-11, abs=1.e-14)
 
     @pytest.mark.skipif(sys.platform.startswith("win"), reason="Does not run on Windows")
     def test_compare2(self, nc):
