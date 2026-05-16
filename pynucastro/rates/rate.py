348be16bec4d53a0547d6a5875c4eb52bc746dbf
Michael Zingale <michael.zingale@stonybrook.edu>
2026-05-16 15:34:39 -0400

    move the full rate evaluation into Rate this will allow us to evaluate the full ρ Y_A Y_B <σv> / (1 + δ_AB) just from the Rate

--- pynucastro/rates/rate.py
+++ pynucastro/rates/rate.py
@@ -1,6 +1,8 @@
 """Classes and methods to interface with files storing rate data."""
 
+import functools
 import math
+from operator import mul
 from pathlib import Path
 
 import numpy as np
@@ -771,6 +773,51 @@ class Rate:
         # 2) A list of log_rates, e.g. ReacLib
         log_rate = np.atleast_1d(self.log_eval(T, rho=rho, comp=comp, screen_func=screen_func))
         return float(np.exp(log_rate).sum())
+
+    def eval_full_rate(self, rho, T, composition, screen_func=None):
+        """Evaluate the rate for a specific density, temperature, and
+        composition, with optional screening.  Note: this returns that
+        rate as dY/dt, where Y is the molar fraction.  For a 2 body
+        reaction, a + b, this will be of the form:
+
+        ρ Y_a Y_b N_A <σv> / (1 + δ_{ab})
+
+        where δ is the Kronecker delta that accounts for a = b.
+
+        If you want dn/dt, where n is the number density (so you get
+        n_a n_b <σv>), then you need to multiply the results here
+        by ρ N_A (where N_A is Avogadro's number).
+
+        Parameters
+        ----------
+        rho : float
+            density used to evaluate rates
+        T : float
+            temperature used to evaluate rates
+        composition : Composition
+            composition used to evaluate rates
+        screen_func : Callable
+            one of the screening functions from :py:mod:`pynucastro.screening`
+            -- if provided, then the evaluated rates will include the screening
+            correction.
+
+        Returns
+        -------
+        float
+
+        """
+
+        ys = composition.get_molar()
+        y_e = composition.ye
+
+        # Note screening effect is already included
+        val = self.prefactor * rho**self.dens_exp * self.eval(T, rho=rho, comp=composition,
+                                                     screen_func=screen_func)
+        if (self.weak_type == 'electron_capture' and
+            not isinstance(self, TabularRate)):
+            val = val * y_e
+        yfac = functools.reduce(mul, [ys[q] for q in r.reactants])
+        return yfac * val
 
     def function_string_py(self):
         """Return a string containing the python function that
