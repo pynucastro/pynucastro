275b026849ee827ef1f36bbee2fbb36daccb0957
zingale <zingale@users.noreply.github.com>
2026-08-24 18:40:15 +0000

    deploy: bb31ea68ec15864ec54d8fc2f89632d4da09805b

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0.0.post17+ge00be34e5',
+    VERSION: '3.0.0.post18+gbb31ea68e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
